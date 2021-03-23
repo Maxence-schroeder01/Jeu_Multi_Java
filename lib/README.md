eaf55e4d30d296e1d98f184b8bd97ce97dcfed2f
Maxence Schroeder <maxence.schroeder@gmail.com>
2021-03-23 15:05:09 -0400

    Update rendu presque final manque update pseudo et doc

---
+++ lib/README.md
@@ -0,0 +1,3 @@
+# projet-initial
+
+Maxence Schroeder
---
+++ lib/serveur via eclipse/MultiNodeSimple/src/App.java
@@ -0,0 +1,7 @@
+
+public class App {
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		MultiNode app = new MultiNode();
+	}
+}
