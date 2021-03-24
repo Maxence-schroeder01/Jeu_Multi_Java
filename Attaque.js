d52730102f7c115d4dfce361ebfad64aeec0789f
Maxence Schroeder <maxence.schroeder@gmail.com>
2021-03-23 22:45:09 -0400

    Maj avant doc

--- Attaque.js
+++ Attaque.js
@@ -6,19 +6,27 @@
     this.multiNode.apprendreAuthentification = (pseudonyme) => this.apprendreAuthentification(pseudonyme);
     this.multiNode.recevoirVariable = (variable) => this.recevoirVariable(variable);
     this.listeJoueur = {};
+    this.activePlayer ="";
+    this.nbrtour = 1;
     this.pseudonymeJoueur = "";
+    this.encore = false;
+    this.conteur = 0;
     this.pseudonymeAutreJoueur = "";
     this.formulaireAuthentification = document.getElementById("formulaire-authentification");
     this.formulaireAuthentification.addEventListener("submit", (evenementsubmit) => this.soumettreAuthentificationJoueur(evenementsubmit))
     this.champPseudonyme = document.getElementById("champ-pseudonyme");
     this.boutonAuthentification = document.getElementById("bouton-authentification");
+    this.boutondespl1 = document.getElementById("bouton-attaquer");
+    this.boutondespl2 = document.getElementById("bouton-attaquer-pl2");
     this.formulaireJeu = document.getElementById("formulaire-jeu");
     this.formulaireJeu.addEventListener("submit", (evenementsubmit) => this.soumettreAttaque(evenementsubmit))
     this.formulaireJeu.style.display = "none";
     this.champPointDeVie = document.getElementById("champ-point-de-vie");
     this.champAttaque = document.getElementById("champ-attaque");
     this.informationAutreJoueur = document.getElementById("information-autre-joueur");
+    this.informationJoueur = document.getElementById("information-joueur");
     this.champPointDeVieAutreJoueur = document.getElementById("champ-point-de-vie-autre-joueur");
+    this.champnbrtour = document.getElementById("nbr-tour");
   }
 
   confirmerConnexion(){
@@ -39,38 +47,64 @@
       this.afficherPartie();
     }
   }
-
   apprendreAuthentification(pseudonyme){
     console.log("Nouvel ami " + pseudonyme);
     this.ajouterJoueur(pseudonyme);
     this.pseudonymeAutreJoueur = pseudonyme;
+    this.quiquiplayenpremier();
     this.afficherPartie();
   }
-
   ajouterJoueur(pseudonyme){
     console.log("ajouterJoueur : " + pseudonyme);
     this.listeJoueur[pseudonyme] = {pointDeVie : Attaque.NOMBRE_POINT_DE_VIE};
   }
 
-  recevoirVariable(variable){
-    console.log("Surcharge de recevoirVariable " + variable.cle + " = " + variable.valeur);
-    let message = JSON.parse(variable.valeur);
-    if(message.pseudonyme == this.pseudonymeJoueur){
-      switch (variable.cle) {
-        case Attaque.MESSAGE.POINT_DE_VIE:
-          this.changerPointdeVieJoueur(message.valeur);
-        break;
-      }
-    }else{
-      switch (variable.cle) {
-        case Attaque.MESSAGE.ATTAQUE:
-          this.subirAttaque(message.valeur);
-        break;
-        case Attaque.MESSAGE.POINT_DE_VIE:
-          this.changerPointdeVieAutreJoueur(message.valeur);
-        break;
-      }
+quiquiplayenpremier(){
+  let despl1 =  Math.floor(Math.random() * 6) + 1; 
+  let despl2 =  Math.floor(Math.random() * 6) + 1; 
+  if (despl1 >= despl2) {
+    this.activePlayer = this.pseudonymeJoueur;
+    console.log(this.pseudonymeJoueur);
+  }
+  else if (despl2 >= despl1) {
+    this.activePlayer = this.pseudonymeAutreJoueur;
+    console.log(this.pseudonymeAutreJoueur);
+  }
+  else{
+    console.log("GG");
+  }
+}
+
+changerplayeractif(){
+  if (this.activePlayer == this.pseudonymeJoueur) {
+    this.activePlayer = this.pseudonymeAutreJoueur;
+    console.log(this.activePlayer);
+  }else{
+    this.activePlayer = this.pseudonymeJoueur;
+    console.log(this.activePlayer);
+  }
+}
+
+
+recevoirVariable(variable){
+  console.log("Surcharge de recevoirVariable " + variable.cle + " = " + variable.valeur);
+  let message = JSON.parse(variable.valeur);
+  if(message.pseudonyme == this.pseudonymeJoueur){
+    switch (variable.cle) {
+      case Attaque.MESSAGE.POINT_DE_VIE:
+        this.changerPointdeVieJoueur(message.valeur);
+      break;
     }
+  }else{
+    switch (variable.cle) {
+      case Attaque.MESSAGE.ATTAQUE:
+        this.subirAttaque(message.valeur);
+      break;
+      case Attaque.MESSAGE.POINT_DE_VIE:
+        this.changerPointdeVieAutreJoueur(message.valeur);
+      break;
+    }
+  }
   }
 
   soumettreAuthentificationJoueur(evenementsubmit){
@@ -85,19 +119,34 @@
   afficherPartie(){
     this.informationAutreJoueur.innerHTML =
       this.informationAutreJoueur.innerHTML.replace("{nom-autre-joueur}", this.pseudonymeAutreJoueur);
+      this.informationJoueur.innerHTML.replace("{nom-joueur}", this.pseudonymeJoueur);
     this.champPointDeVieAutreJoueur.value = this.listeJoueur[this.pseudonymeAutreJoueur].pointDeVie;
     this.champPointDeVie.value = this.listeJoueur[this.pseudonymeJoueur].pointDeVie;
-    this.formulaireJeu.style.display = "block";
+      this.formulaireJeu.style.display = "block";
+    if (this.activePlayer == this.pseudonymeJoueur) {
+      this.boutondespl1.disabled = true;
+      this.boutondespl2.disabled = false;
+    }else{
+      this.boutondespl1.disabled = false;
+      this.boutondespl2.disabled =true;
+    }
   }
 
   genererForceAttaque(){
-    return Math.floor(Math.random() * Attaque.FORCE_MAXIMUM) + 1;
+    let des1 = Math.floor(Math.random() * 6) + 1; 
+    let des2 = Math.floor(Math.random() * 6) + 1;
+    if (des1 == des2) {
+      console.log("Super power activated");
+      this.encore = true;
+    }
+    return des1+des2;
   }
 
   soumettreAttaque(evenementsubmit){
     console.log("soumettreAttaque");
     evenementsubmit.preventDefault();
     let forceAttaque = this.genererForceAttaque();
+    console.log(forceAttaque);
     this.champAttaque.value = forceAttaque;
     let message = {
       pseudonyme : this.pseudonymeJoueur,
@@ -110,7 +159,7 @@
     console.log("subirAttaque()=>valeur" + valeur);
     let message = {
       pseudonyme : this.pseudonymeJoueur,
-      valeur : this.listeJoueur[this.pseudonymeJoueur].pointDeVie - valeur
+      valeur : this.listeJoueur[this.pseudonymeJoueur].pointDeVie + valeur
     };
     this.multiNode.posterVariableTextuelle(Attaque.MESSAGE.POINT_DE_VIE, JSON.stringify(message));
   }
@@ -120,6 +169,7 @@
     this.listeJoueur[this.pseudonymeJoueur].pointDeVie = nouveauPointDeVie;
     this.champPointDeVie.value = nouveauPointDeVie;
     this.validerFinPartie();
+    this.afficherPartie();
   }
 
   changerPointdeVieAutreJoueur(nouveauPointDeVie){
@@ -127,22 +177,34 @@
     this.listeJoueur[this.pseudonymeAutreJoueur].pointDeVie = nouveauPointDeVie;
     this.champPointDeVieAutreJoueur.value = nouveauPointDeVie;
     this.validerFinPartie();
+    this.afficherPartie();
   }
 
   validerFinPartie(){
     console.log("validerFinPartie");
-    if(this.listeJoueur[this.pseudonymeAutreJoueur].pointDeVie <= 0){
+    if(this.listeJoueur[this.pseudonymeAutreJoueur].pointDeVie >= 60){
       alert("Vous avez gagné!");
-    }else if(this.listeJoueur[this.pseudonymeJoueur].pointDeVie <= 0){
+    }else if(this.listeJoueur[this.pseudonymeJoueur].pointDeVie >= 60){
       alert("Vous avez perdu!");
+    }
+    else if(this.encore == true){
+      console.log(this.activePlayer + "a droit a un second tour");
+      this.conteur++;
+      this.nbrtour++;
+      if (this.conteur == 2) {
+        this.conteur = 0;
+        this.changerplayeractif();
+      }
+    }else{
+      this.nbrtour++;
+      this.changerplayeractif();
     }
   }
 
 }
 
 Attaque.NOMBRE_JOUEUR_REQUIS = 2;
-Attaque.NOMBRE_POINT_DE_VIE = 20;
-Attaque.FORCE_MAXIMUM = 5;
+Attaque.NOMBRE_POINT_DE_VIE = 0;
 Attaque.MESSAGE = {
     ATTAQUE : "ATTAQUE",
     POINT_DE_VIE : "POINT_DE_VIE"
--- lib/README.md
+++
@@ -1,3 +0,0 @@
-# projet-initial
-
-Maxence Schroeder
